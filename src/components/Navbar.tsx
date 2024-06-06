d6147131ee394beb4ab9c2e56b9d97538477f663
mrthakur30 <21bec049@nith.ac.in>
2024-06-07 00:44:55 +0530

    done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,31 @@
+import { auth } from '@/utils/firebaseConfig';
 import classNames from 'classnames';
+import { signOut } from 'firebase/auth';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const Navbar : React.FC = () => {
     const pathname = usePathname()
+    const router = useRouter()
+
     const tab = pathname
+
+    const logout = ()=>{
+        signOut(auth);
+        router.push('/login');
+    }
+
     return (
         <div className='h-16 text-xl shadow flex fixed justify-between items-center px-16 w-screen bg-white'>
             <h1 className='text-center font-semibold text-2xl text-pink-500 '>TweetX</h1>
-            <div className='flex gap-4 font-medium justify-center items-center'>
+            <div className='flex gap-6 font-medium justify-center items-center'>
                <Link href='/feed' className={classNames(tab==='/feed' ? 'text-pink-500' : 'text-pink-200')}>Feed</Link>
                <Link href='/users' className={classNames(tab==='/users' ? 'text-pink-500' : 'text-pink-200')}>Users</Link>
                <Link href='/profile' className={classNames(tab==='/profile' ? 'text-pink-500' : 'text-pink-200')}>Profile</Link>
             </div>
+
+            <button className='text-red-500 px-3' onClick={logout}>Log Out</button>
         </div>
     )
 }
